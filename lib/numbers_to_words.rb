ea0234bf55ae4f0c5b25ca8cad3d612af5cc323a
Andrew Brennwald & Kellen Kolbeck <student@epicodus.com>
2015-08-04 16:50:07 -0700

    Algorithm documentation for the infernal machine and end-of-day code

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,6 +1,29 @@
 class Fixnum
   define_method(:numbers_to_words) do
-    corn_beef_hash = {1 => "one"}
+    corn_beef_hash =
+
+      # Teens
+
+      {1 => "one", 2 => "two", 3 => "three",
+       4 => "four", 5  => "five", 6 => "six",
+       7 => "seven", 8 => "eight", 9 => "nine",
+       10 => "ten", 11 => "eleven", 12 => "twelve",
+       13 => "thirteen", 14 => "fourteen", 15=> "fifteen",
+       16 => "sixteen", 17 => "seventeen", 18 => "eighteen",
+       19 => "nineteen",
+
+       # Tens
+
+       20 => "twenty", 30 => "thirty",
+       40 => "fourty", 50 => "fifty", 60 => "sixty",
+       70 => "seventy", 80 => "eighty", 90 => "ninety"}
+
+       # Beyond
+
+       {2 => "hundred", 3 => "thousand", 6 => "million",
+       9 => "billion", 12 => "trillion"}
+
     corn_beef_hash.fetch(self)
+    self.to_s().split('').reverse()
   end
 end
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -2,7 +2,15 @@ require ('rspec')
 require ('numbers_to_words')
 
 describe("Fixnum#numbers_to_words") do
+#Rule 1: Takes single digit number and converts it into written word
   it("Takes a number and translates it into a word version") do
     expect(1.numbers_to_words()).to(eq("one"))
   end
+
+#Rule 2: Translates >20 into a written word
+  it("Takes numbers larger than 20 and translates them into written word form") do
+    expect(21.numbers_to_words()).to(eq("twenty one"))
+  end
+  
+
 end
